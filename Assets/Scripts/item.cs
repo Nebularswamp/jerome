c880978d2a5aecc169016385ffffc73bc0561d31
Massimo Gauthier <massimogauthier1@gmail.com>
2019-02-26 04:04:25 -0500

    created item class

---
+++ Assets/Scripts/item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class item : MonoBehaviour
+{
+    public string itemName = "itemName";
+    public GameObject myPrefab;
+    public int damage = 1;
+
+    public virtual void use() {
+        
+    }
+
+    public virtual void discard() {
+        Instantiate(myPrefab, transform.position, Quaternion.identity);
+    }
+}
