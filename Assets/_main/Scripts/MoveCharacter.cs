3e645612fe877710e602aa38980f6957d30947e6
Massimo Gauthier <massimogauthier1@gmail.com>
2019-02-27 04:38:53 -0500

    changed movement to be relative to camera

--- Assets/_main/Scripts/MoveCharacter.cs
+++ Assets/_main/Scripts/MoveCharacter.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCharacter : MonoBehaviour
+public class MoveCharacter : MonoBehaviour //moves character relative 
 {
 
     public float speed = 5f;
-    
+
+    GameObject myCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myCamera = Camera.main.gameObject;
     }
 
     // Update is called once per frame
@@ -18,8 +19,16 @@ public class MoveCharacter : MonoBehaviour
     {
         float horizontalDirection = Input.GetAxisRaw("Horizontal");
         float verticalDirection = Input.GetAxisRaw("Vertical");
-        Vector3 moveDirection = new Vector3(horizontalDirection, 0, verticalDirection).normalized;
 
+        //vars for moving relative to camera
+        Vector3 camR = myCamera.transform.right;
+        Vector3 camF = myCamera.transform.forward;
+        camR.y = 0;
+        camF.y = 0;
+        camR = camR.normalized;
+        camF = camF.normalized;
+
+        Vector3 moveDirection = (horizontalDirection * camR + verticalDirection * camF).normalized;
         transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
 
     }
